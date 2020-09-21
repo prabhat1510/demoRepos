5b790a5d9fbcc0364d8702df4978a9c4b1ed0333
prabhat1510 <prabhatc@gmail.com>
2020-09-21 09:09:59 +0530

    Created HelloWorld Java File
    Commit helloWorld Java

---
+++ demo/src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello ");
+	}
+
+}
